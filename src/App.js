37217a21570f0cb8abe7704c4f992f9a1f7343f1
Sina Sadrzadeh <sadrzadeh.sinaa@gmail.com>
2021-01-14 08:25:14 +0330

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo" width="100px" />
+        <p></p>
+        <h4>Frontend Technical Interview</h4>
       </header>
+      <div className="Divider">&nbsp;</div>
+      <p></p>
+      <div class="card">
+        <div class="card-body">
+          CODE HERE
+        </div>
+      </div>
     </div>
   );
 }
